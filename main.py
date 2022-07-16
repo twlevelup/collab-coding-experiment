03ad7127452d52c5f492afbf7c5340bdce5e8400
jarryd-harris <jarryd.harris@thoughtworks.com>
2022-07-16 21:15:10 +1000

    create a layout for game loop

--- main.py
+++ main.py
@@ -1,4 +1,63 @@
+import words.word_bank as wb
+import os
+from rich import print
+from rich.panel import Panel
+from rich.padding import Padding
+from rich.align import Align
+
+WELCOMETEXT = """
+[white]Welcome to [red]LevelUp 2022[white]!
+Lets play a game of Hangman!
+Start by entering your name.
+"""
+
+WORDLENGTHTEXT = """
+[white]Hi [red]{}[white], now choose how long you want your word to be[3-20].
+"""
+
+WORDLENGTHERROR = """
+[white]Please enter an [red]integer [white]from 3 to 20 
+"""
+
+def game_box(text):
+    os.system('clear')
+    print(
+        Panel(
+            Padding(
+                Align(
+                    text,
+                    align='center',
+                    vertical='middle'
+                    ), 
+                3), 
+            title="Hangman LevelUp 2022",
+            height=15)
+        )
+
+def is_valid_word_length(string):
+    if not string.isdigit():
+        return False
+    if int(string) not in range(3,21):
+        return False
+    return True
+
 def main():
+
+    game_box(WELCOMETEXT)
+    name = input()
+
+    game_box(WORDLENGTHTEXT.format(name))
+    word_length = input()
+
+    while not is_valid_word_length(word_length):
+        game_box(WORDLENGTHERROR)
+        word_length = input()
+
+    word_length = int(word_length)
+    word_bank = wb.WordBank()
+    word = word_bank.get_word(word_length)
+    game_box(word)
+
     return
 
 if __name__ == '__main__':
