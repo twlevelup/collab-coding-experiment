fa361eedc1e3983ed2b59d8dd5cd0c3fe5ade1f5
jarryd-harris <jarryd.harris@thoughtworks.com>
2022-07-16 16:34:12 +1000

    add word generator with length filter

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hangman
+
+Simple hangman app
---
+++ words/word_bank.py
@@ -0,0 +1,13 @@
+from random import choice
+
+class WordBank:
+    def __init__(self):
+        self.word_list = self.generate_word_list()
+
+    def generate_word_list(self, path="./words/words.txt"):
+        with open(path, "r") as file:
+            words = [line.strip("\n").lower() for line in file.readlines()]
+        return words
+
+    def get_word(self, word_length):
+        return choice([word for word in self.word_list if len(word) <= word_length])
